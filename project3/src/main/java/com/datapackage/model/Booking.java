b2f6534885beeee8bf8092ac23931a7557bf9131
Kalani Rajapaksha <132144157+kalaniNR@users.noreply.github.com>
2025-02-21 12:21:05 +0530

    Add files via upload

---
+++ project3/src/main/java/com/datapackage/model/Booking.java
@@ -0,0 +1,57 @@
+package com.datapackage.model;
+
+public class Booking {
+	
+	private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String fromLocation;
+    private String toLocation;
+    private String datetime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getFromLocation() {
+		return fromLocation;
+	}
+	public void setFromLocation(String fromLocation) {
+		this.fromLocation = fromLocation;
+	}
+	public String getToLocation() {
+		return toLocation;
+	}
+	public void setToLocation(String toLocation) {
+		this.toLocation = toLocation;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+    
+    
+
+}
